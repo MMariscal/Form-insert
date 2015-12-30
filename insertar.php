91ab5300f053c160f7e73f996b49c074f90a761c
Marcos Mariscal <marcos.mariscal@icloud.com>
2015-12-30 13:56:45 +0100

    Formulario de entrada de datos

---
+++ insertar.php
@@ -0,0 +1,20 @@
+<HTML>
+    <HEAD>
+        <TITLE>Insertar.php</TITLE>
+    </HEAD>
+    <BODY>
+        <?
+        //Conexion con la base
+        mysql_connect("localhost","root","");
+
+        //selección de la base de datos con la que vamos a trabajar
+        mysql_select_db("NTCSerie22");
+
+        //Ejecucion de la sentencia SQL
+        mysql_query("INSERT INTO `pieza` (`idPIEZA`, `MODELO`, `MEDIDAS`, `USO`, `SERIE`, `COLOR`, `APLICACION`, `ESTILO`, `IMAGEN`, `OTROS`) VALUES (NULL, '$modelo', '$medidas', '$uso', '$serie', '$color', '$aplicacion', '$estilo', NULL, NULL)");
+
+        ?>
+        <h1><div align="center">Registro Insertado</div></h1>
+        <div align="center"><a href="lectura.php">Visualizar el contenido de la base</a></div>
+    </BODY>
+</HTML>
